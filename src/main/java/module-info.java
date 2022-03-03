ef24189c5b0941e56dc065fed038981015badb6d
olovm <olov.mckie@ub.uu.se>
2022-03-03 13:37:53 +0000

    remove module info, no point with modules and shaded jar

--- src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module se.uu.ub.diva.cora.fitnesse {
-	requires org.apache.commons.text;
-	requires se.uu.ub.cora.data;
-	requires se.uu.ub.cora.classicfedorasynchronizer;
-	requires se.uu.ub.cora.xmlutils;
-}
